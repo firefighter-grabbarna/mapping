5ac78253acd5c31fb42605c9c6c71fb7670ae8d5
anton <antos931@student.liu.se>
2022-02-10 18:45:04 +0100

    added functions to move and started work on radio control

--- src/motor/omniwheels_movement/src/main.cpp
+++ src/motor/omniwheels_movement/src/main.cpp
@@ -1,153 +1,170 @@
-#include <Wire.h>
-#include <SoftwareSerial.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+#include <AFMotor.h>
+#include <Arduino.h>
 
-// TODO: The wheels will need to be defined corretly
-Adafruit_DCMotor *LeftFrontWheel  = AFMS.getMotor(1);
-Adafruit_DCMotor *RightFrontWheel   = AFMS.getMotor(2); 
-Adafruit_DCMotor *LeftBackWheel = AFMS.getMotor(3);
-Adafruit_DCMotor *RightBackWheel  = AFMS.getMotor(4);
+const int PWM_PIN = 10;
+const int standStill = 255/2;
 
-int defaultSpeed = 60;
+AF_DCMotor RightBackWheel(4);
+AF_DCMotor LeftBackWheel(3);
+AF_DCMotor LeftFrontWheel(2);
+AF_DCMotor RightFrontWheel(1);
 
 void setup() {
+   Serial.begin(115200);
+   Serial.println("Motor test!");
 
-  Serial.begin(115200);
-  // put your setup code here, to run once:
-  AFMS.begin();
-  
-  LeftFrontWheel->setSpeed(defaultSpeed);
-  LeftBackWheel->setSpeed(defaultSpeed);
-  
-  RightBackWheel->setSpeed(defaultSpeed);
-  RightFrontWheel->setSpeed(defaultSpeed);
+   // turn on motor dont crash code
+   RightBackWheel.run(RELEASE);
+   LeftBackWheel.run(RELEASE);
+   LeftFrontWheel.run(RELEASE);
+   RightFrontWheel.run(RELEASE);
 
-  delay(20);
-  
 }
 
 void loop() {
-  
-  // put your main code here, to run repeatedly:
+
+   int pwmValue = pulseIn(PWM_PIN, HIGH);
+   bool movingForward = pwmValue > standStill;
+   bool movingForward = pwmValue < standStill;
+   int threshold = 10;
+   if (standStill - threshold < pwmValue < standStill + threshold){
+      setSpeed(standStill);
+   }
+   else if (moveForward){
+      setSpeed(pwmValue);
+      moveForward();
+   }
+   else if (moveBackward){
+      setSpeed(pwmValue);
+      moveBackward();
+   }
+}
+
+int forward(int speed){
+   return standStill - speed;
+}
+
+int backward(int speed){
+   return standStill + speed;
+}
+void setSpeed(int speed){
+
+   RightBackWheel.setSpeed(speed);
+   LeftBackWheel.setSpeed(speed);
+   LeftFrontWheel.setSpeed(speed);
+   RightFrontWheel.setSpeed(speed);
 
 }
+
 void moveForward()
 {
-   LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(FORWARD);
+   LeftFrontWheel.run(FORWARD);
+   LeftBackWheel.run(FORWARD);
    
-   RightFrontWheel->run(FORWARD); 
-   RightBackWheel->run(FORWARD);
+   RightFrontWheel.run(FORWARD); 
+   RightBackWheel.run(FORWARD);
   
 }
 void moveBackward() {
   
-   LeftFrontWheel->run(BACKWARD);
-   LeftBackWheel->run(BACKWARD);
+   LeftFrontWheel.run(BACKWARD);
+   LeftBackWheel.run(BACKWARD);
    
-   RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(BACKWARD);
+   RightFrontWheel.run(BACKWARD);
+   RightBackWheel.run(BACKWARD);
    
 }
 void moveSidewaysRight() {
 
-   LeftFrontWheel->setSpeed(120);
-   LeftBackWheel->setSpeed(120);
+   LeftFrontWheel.setSpeed(120);
+   LeftBackWheel.setSpeed(120);
   
-   RightBackWheel->setSpeed(120);
-   RightFrontWheel->setSpeed(120);
+   RightBackWheel.setSpeed(120);
+   RightFrontWheel.setSpeed(120);
 
-   LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(BACKWARD);
+   LeftFrontWheel.run(FORWARD);
+   LeftBackWheel.run(BACKWARD);
 
-   RightFrontWheel->run(FORWARD);
-   RightBackWheel->run(BACKWARD);
+   RightFrontWheel.run(FORWARD);
+   RightBackWheel.run(BACKWARD);
    
 }
 void moveSidewaysLeft() {
 
-   LeftFrontWheel->setSpeed(120);
-   LeftBackWheel->setSpeed(100);
+   LeftFrontWheel.setSpeed(120);
+   LeftBackWheel.setSpeed(100);
 
-   RightFrontWheel->setSpeed(100);
-   RightBackWheel->setSpeed(120);
+   RightFrontWheel.setSpeed(100);
+   RightBackWheel.setSpeed(120);
    
   
-   LeftFrontWheel->run(BACKWARD);
-   LeftBackWheel->run(FORWARD);
+   LeftFrontWheel.run(BACKWARD);
+   LeftBackWheel.run(FORWARD);
    
-   RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(FORWARD);
+   RightFrontWheel.run(BACKWARD);
+   RightBackWheel.run(FORWARD);
    
 }
 void rotateLeft() {
   
-   LeftFrontWheel->run(BACKWARD);
-   LeftBackWheel->run(BACKWARD);
+   LeftFrontWheel.run(BACKWARD);
+   LeftBackWheel.run(BACKWARD);
    
-   RightFrontWheel->run(FORWARD);
-   RightBackWheel->run(FORWARD);
+   RightFrontWheel.run(FORWARD);
+   RightBackWheel.run(FORWARD);
    
 }
 void rotateRight() {
   
-   LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(FORWARD);
+   LeftFrontWheel.run(FORWARD);
+   LeftBackWheel.run(FORWARD);
    
-   RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(BACKWARD);
+   RightFrontWheel.run(BACKWARD);
+   RightBackWheel.run(BACKWARD);
    
 }
 void moveRightForward() {
   
-   LeftFrontWheel->run(FORWARD);
-  // LeftBackWheel->run(FORWARD);
+   LeftFrontWheel.run(FORWARD);
+  // LeftBackWheel.run(FORWARD);
    
- //  RightFrontWheel->run(BACKWARD);
-   RightBackWheel->run(BACKWARD);
+ //  RightFrontWheel.run(BACKWARD);
+   RightBackWheel.run(BACKWARD);
    
 }
 void moveRightBackward() {
   
-  // LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(BACKWARD);
+  // LeftFrontWheel.run(FORWARD);
+   LeftBackWheel.run(BACKWARD);
    
-   RightFrontWheel->run(BACKWARD);
- //  RightBackWheel->run(BACKWARD);
+   RightFrontWheel.run(BACKWARD);
+ //  RightBackWheel.run(BACKWARD);
    
 }
 void moveLeftForward() {
-  // LeftFrontWheel->run(FORWARD);
-   LeftBackWheel->run(FORWARD);
+  // LeftFrontWheel.run(FORWARD);
+   LeftBackWheel.run(FORWARD);
    
-   RightFrontWheel->run(FORWARD);
- //  RightBackWheel->run(BACKWARD);
+   RightFrontWheel.run(FORWARD);
+ //  RightBackWheel.run(BACKWARD);
 }
 void moveLeftBackward() {
   
-   LeftFrontWheel->run(BACKWARD);
-  // LeftBackWheel->run(FORWARD);
+   LeftFrontWheel.run(BACKWARD);
+  // LeftBackWheel.run(FORWARD);
    
- //  RightFrontWheel->run(FORWARD);
-   RightBackWheel->run(BACKWARD);
+ //  RightFrontWheel.run(FORWARD);
+   RightBackWheel.run(BACKWARD);
 }
 void motorStop(){
   
-   LeftFrontWheel->run(RELEASE);
-   LeftBackWheel->run(RELEASE);
+   LeftFrontWheel.run(RELEASE);
+   LeftBackWheel.run(RELEASE);
    
-   RightFrontWheel->run(RELEASE);
-   RightBackWheel->run(RELEASE);
-}
-
-void setSpeed()
-{
-   LeftFrontWheel->setSpeed(defaultSpeed);
-  LeftBackWheel->setSpeed(defaultSpeed);
-  
-  RightBackWheel->setSpeed(defaultSpeed);
-  RightFrontWheel->setSpeed(defaultSpeed);
+   RightFrontWheel.run(RELEASE);
+   RightBackWheel.run(RELEASE);
 }
