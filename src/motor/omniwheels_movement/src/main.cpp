1a596bc9e23590b6e1e7d3239e0820a6c2e3c578
antos931 <antos931@student.liu.se>
2022-02-13 02:16:37 +0100

    Added movement in any direction, however untested

--- src/motor/omniwheels_movement/src/main.cpp
+++ src/motor/omniwheels_movement/src/main.cpp
@@ -8,10 +8,22 @@ const int PWM_PIN = A0;
 const int standStill = 1500; // Controller in middle position
 const int BAUD_RATE = 9600;
 
-AF_DCMotor RightBackWheel(3);
-AF_DCMotor LeftBackWheel(2);
-AF_DCMotor LeftFrontWheel(4);
+
+
+
+const int RIGHT_FRONT_WHEEL = 1;
+const int LEFT_BACK_WHEEL = 2;
+const int RIGHT_BACK_WHEEL = 3;
+const int LEFT_FRONT_WHEEL = 4;
+
+const int SPEED_THRESHOLD = 10;
+
+
 AF_DCMotor RightFrontWheel(1);
+AF_DCMotor LeftBackWheel(2);
+AF_DCMotor RightBackWheel(3);
+AF_DCMotor LeftFrontWheel(4);
+
 
 void setup() {
    //115200
@@ -25,8 +37,139 @@ void setup() {
    RightFrontWheel.run(RELEASE);
 
 }
+/*
+param - omega is the angular speed
+param - x is the speed in x-direction
+param - y is the speed in y-direction
+
+NOTE: since the matrix is of dimension 4 an arbitrary choice of u1,u2,u3,u4 can cause skidding
+      (it is belived to be what it means)
+      These vidoes are of concern:
+      How the speed is calculated:
+https://www.youtube.com/watch?v=NcOT9hOsceE
+
+      This talks about how to limit of our speed needs to be taken into account:
+      (one could just do low enought speed and this would probably not be a problem)
+https://www.youtube.com/watch?v=B1K-ti5Lqjc
+
+*/
+
+void getWheelSpeeds(double x, double y, double omegaZ, double (&speedVector)[4]){
+   /* 
+   l is the distance from the middle of the base to the middle of the wheel
+   w is the distance from the wheel to the middle of the base.
+   These distances are not diagonal, only the straight line on the x or y axis
+   */ 
+   double l = -1; // TODO: update these to the correct value 
+   double w = -1;
+   double r = -1; // the radius of the wheel
+   /*
+   This is the resulting vector from matrix mutliplication of
+           
+      u1    -l-w   1  -1     
+      u2  =  l+w   1   1  *   w = H(0)*vb
+      u3     l+w   1  -1      x
+      u4    -l-w   1   1      y
+                          
+   */
+   double u1 = (1/r) * ( (-l - w) * omegaZ + x - y); 
+   double u2 = (1/r) * ( (l + w) * omegaZ + x + y);
+   double u3 = (1/r) * ( (l + w) * omegaZ + x - y);
+   double u4 = (1/r) * ( (-l - w) * omegaZ + x + y);
+   // the wheels should be mapped correctly, however test this to make sure
+   speedVector[0] = u2; // right front
+   speedVector[1] = u4; // left back
+   speedVector[2] = u3; // right back
+   speedVector[3] = u1; // left front
+}
+/*
+
+   Given a speed and a wheel 
+   sets the correct direction for the wheel
+
+   A negative speed means backwards
+   A positive speed means fortwads
+*/
+
+void setSpeedAndDirection(int speed, AF_DCMotor &wheel){ 
+   if (speed >= SPEED_THRESHOLD){
+      wheel.run(FORWARD);
+      wheel.setSpeed(speed);
+   }
+   else if (speed <= -SPEED_THRESHOLD){
+      wheel.run(BACKWARD);
+      wheel.setSpeed(abs(speed));
+   }
+
+}
+
+/*
+
+   Used in for example a for- loop where an integer makes an action on the 
+   corresponding wheel
+*/
+
+void setWheelSpeed(int wheel_case, int speed){
+   switch (wheel_case)
+   {
+   case LEFT_BACK_WHEEL:
+         setSpeedAndDirection(speed, LeftBackWheel);
+      break;
+   case RIGHT_BACK_WHEEL:
+         setSpeedAndDirection(speed, RightBackWheel);
+      break;
+   case LEFT_FRONT_WHEEL:
+         setSpeedAndDirection(speed, LeftFrontWheel);
+      break;
+   case RIGHT_FRONT_WHEEL:
+         setSpeedAndDirection(speed, RightFrontWheel);
+      break;
+   }
+}
+/*
+   Sets the speed of each wheel to the elements of u
+*/
+void setWheelSpeed(double (&u)[4]){
+
+   // make sure the wheels are mapped to the correct for this to work!
+   for (int i = 1; i == sizeof(u) ; i++){
+      setWheelSpeed(i, u[i]);
+   }
+}
+
 
 void loop() {
+   // ---------------------------
+   // this code is untested and therefore commented until it can be tested
+   // DO NOT REMOVE as i this is to be tested
+   // -----------------------------
+   /*
+   
+   double speedVector[4];
+   // this should move the car in x-direction
+   getWheelSpeeds(100, 0, 0, speedVector);
+   setWheelSpeed(speedVector);
+   // this should move the car in y-direction
+   getWheelSpeeds(0, 100, 0, speedVector);
+   setWheelSpeed(speedVector);
+   // this should spin the car in a circle
+   getWheelSpeeds(0, 0, 50, speedVector);
+   setWheelSpeed(speedVector);
+   // this should spin the car in a circle the other direction
+   getWheelSpeeds(0, 0, -50, speedVector);
+   setWheelSpeed(speedVector);
+   // this should move diagonal
+   getWheelSpeeds(50, 50, 0, speedVector);
+   setWheelSpeed(speedVector);
+   // this should spin the car while moving y - direction
+   getWheelSpeeds(0, 50, 50, speedVector);
+   setWheelSpeed(speedVector);
+   // this should spin the car while moving x and y - direction
+   getWheelSpeeds(40, 40, 40, speedVector);
+   setWheelSpeed(speedVector);
+   
+   */
+
 
    int pwmValue = pulseIn(PWM_PIN, HIGH); // Read controller
    Serial.println(pwmValue);
