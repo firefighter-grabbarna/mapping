3fe9adb7b59c88b5e82fdb2cb20c119e98ec1ce5
Alexander Berntsson <alexande.berntsson@gmail.com>
2022-03-17 20:41:05 +0100

    fixed and tested scanner for y axis.

--- src/canon/FirefighterScan/src/main.cpp
+++ src/canon/FirefighterScan/src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <Servo.h> 
 
-static int X_SERVO_PIN = 9;
+static int Y_SERVO_REST_POS = 60;
+static int Y_SERVO_VERTICAL_OFFSET = -45;
+static int X_SERVO_PIN = 3;
 static int X_FLAME_PIN = A0;
-static int Y_SERVO_PIN = 10;
-static int Y_FLAME_PIN = A1;
+static int Y_SERVO_PIN = 5;
+static int Y_FLAME_PIN = A0;
 Servo xAxisServo;
 Servo yAxisServo;
 void setup() {
@@ -29,14 +31,14 @@ void setup() {
   * 
   */  
 
-int getSmallestReading(Servo& servo, int flamePin){
-  servo.write(0);
+
+int getSmallestReading(Servo& servo, int flamePin, int minDegree, int maxDegree){
+  servo.write(minDegree);
     
-  delay(1000);
-  int maxDegree = 180;
+  delay(400);
   int nearestDegree = 0;
   int smallestReading = 90000;
-  for (int degree = 0; degree < maxDegree; degree++){
+  for (int degree = minDegree; degree < maxDegree; degree++){
     int reading = analogRead(flamePin);
     Serial.println(reading);
     servo.write(degree);
@@ -49,6 +51,12 @@ int getSmallestReading(Servo& servo, int flamePin){
   return nearestDegree;
 } 
 
+
+
+int getSmallestReading(Servo& servo, int flamePin){
+  getSmallestReading(servo, flamePin, 0, 180);
+} 
+
 /*
   Moves the servo to the degree which returns the smallest
   reading from the flame sensor 
@@ -56,12 +64,17 @@ int getSmallestReading(Servo& servo, int flamePin){
   Smallest reading is the one which is closest to the flame
 */
 
-void moveToLargestReading(Servo& servo, int flamePin){
+void moveToLargestReading(Servo& servo, int flamePin, int minDegree, int maxDegree) {
   delay(100);
-  int degree = getSmallestReading(servo, flamePin);
+  int degree = getSmallestReading(servo, flamePin, minDegree, maxDegree);
   servo.write(degree);
-  Serial.println(degree);
-  delay(2000);
+  //Serial.println(degree);
+  delay(400);
+
+}
+
+void moveToLargestReading(Servo& servo, int flamePin){
+  moveToLargestReading(servo, flamePin, 0, 180);
 
 }
 
@@ -73,8 +86,13 @@ void loop() {
    * get the servo degree for the largest value,
    * then return the servo to the greatest degree.
    */
-
-  moveToLargestReading(xAxisServo, X_FLAME_PIN);
-  moveToLargestReading(yAxisServo, Y_FLAME_PIN);
-}
-
+   if (Serial.available()) {
+    String s = Serial.readStringUntil('\n');
+    yAxisServo.write(Y_SERVO_REST_POS);
+    moveToLargestReading(xAxisServo, X_FLAME_PIN);
+    xAxisServo.write(xAxisServo.read()+Y_SERVO_VERTICAL_OFFSET);
+    delay(300);
+    moveToLargestReading(yAxisServo, Y_FLAME_PIN,40, 120);
+    xAxisServo.write(xAxisServo.read()-Y_SERVO_VERTICAL_OFFSET);  
+   }
+  }
