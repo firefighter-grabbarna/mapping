7f447ae1eafca3896a73f0258d6e9097ba593327
Herman Lauenstein <herla723@student.liu.se>
2022-03-31 18:39:52 +0200

    Test serial

--- src/robot/main.cpp
+++ src/robot/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 
 #include "../common/lidar.hpp"
-
-using namespace std;
+#include "../common/serial.hpp"
 
 int main() {
-    Lidar lidar("/dev/ttyACM0");
+    Serial serial("/dev/ttyAMA0");
 
-    while (true) {
-        auto values = lidar.scan();
-        for (int v : values) {
-            std::cout << v << " ";
-        }
-        std::cout << std::endl << std::endl;
+    std::string line;
+
+    bool on = false;
+
+    while (getline(std::cin, line)) {
+        // do something with the line
+
+        on = !on;
+        serial.output(on ? "1\n" : "0\n");
+        std::cout << "W: " << (int) on << std::endl;
     }
+
+    //serial.output("1\n");
+
+    // Lidar lidar("/dev/ttyACM0");
+
+    // while (true) {
+    //     auto values = lidar.scan();
+    //     for (int v : values) {
+    //         std::cout << v << " ";
+    //     }
+    //     std::cout << std::endl << std::endl;
+    // }
 }
